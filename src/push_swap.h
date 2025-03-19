fe64e5932c2c8f726d4d29965b831e9d758cdd1e
erico-ke <eliasricokergaravat@gmail.com>
2025-03-19 19:28:11 +0100

    Added push

--- src/push_swap.h
+++ src/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: erico-ke <erico-ke@42malaga.student.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 14:16:11 by erico-ke          #+#    #+#             */
-/*   Updated: 2025/03/19 19:09:01 by erico-ke         ###   ########.fr       */
+/*   Updated: 2025/03/19 19:27:10 by erico-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,9 +34,16 @@ int		print_error(void);
 
 /* Utils */
 long	ft_atol(char *nptr);
-int		ft_lst_size(t_stack *lst);
 
 /* List management */
 t_stack	*lst_new(int content);
+int		ft_lst_size(t_stack *lst);
+
+/* Movements */
+t_stack	*sa(t_stack *a);
+t_stack	*sb(t_stack *b);
+void	ss(t_push_swap	*lst);
+void	pa(t_push_swap	*lst);
+void	pb(t_push_swap	*lst);
 
 #endif
