afbff53e115da6b066f2598e45db099d83fe352b
erico-ke <eliasricokergaravat@gmail.com>
2025-03-19 21:04:15 +0100

    En el parseo si meto duplicados o numeros que sobrepasan el MAX_INT se los traga sin problema y no de error

--- src/moves/r_rotate.c
+++ src/moves/r_rotate.c
@@ -6,11 +6,11 @@
 /*   By: erico-ke <erico-ke@42malaga.student.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 19:47:46 by erico-ke          #+#    #+#             */
-/*   Updated: 2025/03/19 20:58:03 by erico-ke         ###   ########.fr       */
+/*   Updated: 2025/03/19 21:00:40 by erico-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include "../push_swap.h"
 
 static t_stack	*r_rotate(t_stack *stack)
 {
