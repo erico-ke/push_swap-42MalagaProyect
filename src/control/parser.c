a33506cc88ee2bea969e8e3be568e2ff9e865399
erico-ke <eliasricokergaravat@gmail.com>
2025-03-19 21:07:58 +0100

    cambio menor

--- src/control/parser.c
+++ src/control/parser.c
@@ -6,7 +6,7 @@
 /*   By: erico-ke <erico-ke@42malaga.student.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 17:53:03 by erico-ke          #+#    #+#             */
-/*   Updated: 2025/03/19 21:07:32 by erico-ke         ###   ########.fr       */
+/*   Updated: 2025/03/19 21:07:49 by erico-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,7 +92,7 @@ int	ft_checkforvalidlst(t_stack *a)//Esta funcion no esta funcionando.... ¿PORQ
 	while (a->next != NULL)
 	{
 		tmp = a->next;
-		if (a->content > INT_MAX || a ->content < INT_MIN)
+		if (a->content > INT_MAX || a->content < INT_MIN)
 			return (EXIT_FAILURE);
 		while(tmp->next != NULL)
 		{
